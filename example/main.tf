c17dc23373347d56c24548b0ba2fabb3ebc86900
himanshiparnami <101627875+himanshiparnami@users.noreply.github.com>
2022-05-06 19:14:58 +0530

    Update main.tf

--- example/main.tf
+++ example/main.tf
@@ -1,17 +1,16 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=2.99.0"
-    }
-  }
-}
-
-# Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
 }
 
+module "resource_group" {
+  source                  = "OT-terraform-azure-modules/resource-group/azure"
+  resource_group_name     = "aks-rg"
+  resource_group_location = "australiaeast"
+
+  tag_map = {
+    Name = "AKSRG"
+  }
+}
 
 module "ssh-key" {
   source         = "./key"
@@ -19,60 +18,39 @@ module "ssh-key" {
 }
 
 module "aks" {
-  source                               = "../"
-  key_data                             = replace(var.public_ssh_key == "" ? module.ssh-key.public_ssh_key : var.public_ssh_key, "\n", "")
-  resource_group_name                  = "example_rg"
-  location                             = "Australia East"
-  cluster_name                         = "testing-aks-cluster"
-  cluster_log_analytics_workspace_name = "Workspace-65775"
-  prefix                               = "prefix"
-  client_id                            = ""
-  client_secret                        = ""
-  admin_username                       = "azureuser"
-  agents_size                          = "Standard_D2s_v3"
-  log_analytics_workspace_sku          = "PerGB2018"
-  log_retention_in_days                = 30
-  agents_count                         = 2
-  tags                                 = var.tags
-  enable_log_analytics_workspace       = true
-  #vnet_subnet_id                      = "null"
-  os_disk_size_gb                      = "50"
-  private_cluster_enabled              = true
-  enable_kube_dashboard                = false
-  enable_http_application_routing      = false
-  enable_azure_policy                  = false
-  sku_tier                             = "Free"
-  enable_role_based_access_control     = true
-  rbac_aad_managed                     = true
-  rbac_aad_admin_group_object_ids      = null
-  rbac_aad_client_app_id               = ""
-  rbac_aad_server_app_id               = ""
-  rbac_aad_server_app_secret           = ""
-  network_policy                       = "calico"
-  network_plugin                       = "kubenet"
-  net_profile_dns_service_ip           = "10.0.0.10"
-  net_profile_docker_bridge_cidr       = "170.10.0.1/16"
-  net_profile_outbound_type            = "loadBalancer"
-  net_profile_pod_cidr                 = "170.0.0.0/16"
-  net_profile_service_cidr             = "10.0.0.0/16"
-  kubernetes_version                   = "1.22.6"
-  orchestrator_version                 = "1.22.6"
-  enable_auto_scaling                  = false
-  agents_max_count                     = 2
-  agents_min_count                     = 1
-  agents_pool_name                     = "nodepool"
-  enable_node_public_ip                = false
-  agents_availability_zones            = ["1", "2"]
-  agents_labels                        = { "nodepool" : "defaultnodepool" }
-  agents_type                          = "VirtualMachineScaleSets"
-  agents_tags                          = var.agents_tags
-  agents_max_pods                      = 100
-  enable_ingress_application_gateway   = null
-  ingress_application_gateway_id          = ""
-  ingress_application_gateway_name        = ""
-  ingress_application_gateway_subnet_cidr = ""
-  ingress_application_gateway_subnet_id   = ""
-  identity_type                        = "SystemAssigned"
-  user_assigned_identity_id            = "null"
-  enable_host_encryption               = false
+  source                                    = "../"
+  key_data                                  = replace(var.public_ssh_key == "" ? module.ssh-key.public_ssh_key : var.public_ssh_key, "\n", "")
+  resource_group_name                       = module.res_group.resource_group_name
+  location                                  = module.res_group.resource_group_location
+  cluster_name                              = "testing-aks-cluster"
+  cluster_log_analytics_workspace_name      = "Workspace-65775"
+  prefix                                    = "prefix"
+  admin_username                            = "azureuser"
+  agents_size                               = "Standard_D2s_v3"
+  log_analytics_workspace_sku               = "PerGB2018"
+  log_retention_in_days                     = 30
+  agents_count                              = 2
+  tags                                      = var.tags
+  os_disk_size_gb                           = "50"
+  sku_tier                                  = "Free"
+  enable_role_based_access_control          = true
+  rbac_aad_managed                          = true
+  network_policy                            = "calico"
+  network_plugin                            = "kubenet"
+  net_profile_dns_service_ip                = "10.0.0.10"
+  net_profile_docker_bridge_cidr            = "170.10.0.1/21"
+  net_profile_outbound_type                 = "loadBalancer"
+  net_profile_pod_cidr                      = "170.0.0.0/21"
+  net_profile_service_cidr                  = "10.0.0.0/21"
+  kubernetes_version                        = "1.22.6"
+  orchestrator_version                      = "1.22.6"
+  agents_max_count                          = 2
+  agents_min_count                          = 1
+  agents_pool_name                          = "nodepool"
+  agents_availability_zones                 = ["1", "2"]
+  agents_labels                             = { "nodepool" : "defaultnodepool" }
+  agents_type                               = "VirtualMachineScaleSets"
+  agents_tags                               = var.agents_tags
+  agents_max_pods                           = 100
+  identity_type                             = "SystemAssigned"
 }
